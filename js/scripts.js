999a19dc7bdab9a25cac1a43f2ed6187a236b29a
Jared Lutz and Matt Nardoni <jared@>
2018-04-04 16:25:17 -0700

    refactor back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
 var finalTotal = [];
 var playerTotal = [];
 var turnTotal = [];
+//change this VVVV
+function Athlete(title) {
+  this.title = title;
+  this.diceRoll= turnTotal;
+  this.hold = playerTotal;
+  this.score = finalTotal;
+}
+//change this ^^^
+console.log(Athlete);
+
+function diceRoll(min, max) {
+  var roll = Math.random() * (6 - 1) + 1;
+  var result = Math.round(roll);
+  if (result === 1) {
+    turnTotal.length = 0;
+  } else {
+    turnTotal.push(result);
+  }
+}
+
+function hold() {
+  var total=0;
+  for(i = 0; i < turnTotal.length; i++){
+    total += turnTotal[i];
+  }
+  return playerTotal.push(total);
+}
 
 function score() {
   var total=0;
@@ -11,29 +38,6 @@ function score() {
   if (result < 100) {
     finalTotal.length = 0;
   } else {
-    alert("Congratulations!");
     return finalTotal.push(total);
   }
-}
-
-
-function hold() {
-  var total=0;
-  for(i = 0; i < turnTotal.length; i++){
-    total += turnTotal[i];
-  }
-  return playerTotal.push(total);
-
-}
-
-function diceRoll(min, max) {
-  var roll = Math.random() * (6 - 1) + 1;
-  var result = Math.round(roll);
-  if (result === 1) {
-    turnTotal.length = 0;
-  } else {
-    turnTotal.push(result);
-  }
-  console.log(result);
-  console.log(turnTotal);
 }
