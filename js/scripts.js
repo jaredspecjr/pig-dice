6832cab20622355b76f42066eefdd367fd046c3f
Jared Lutz and Matt Nardoni <jared@>
2018-04-05 16:33:54 -0700

    finish refactoring back-end, add front-end logic and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 
+
 function Athlete(title) {
   this.title = title;
-  this.roll = [];
+  this.roll = 0;
   this.playerScore = 0;
   this.turnTotal = 0;
 }
 
 
-var player1 = new Athlete("jared");
-var player2 = new Athlete("matt");
+
+
 Athlete.prototype.diceRoll = function() {
   var random = Math.random() * (6 - 1) + 1;
    this.roll = Math.round(random);
@@ -22,8 +23,7 @@ Athlete.prototype.diceRoll = function() {
 
 
   }
-  console.log(player1.roll);
-  console.log(player2.roll);
+
 }
 
 Athlete.prototype.hold = function() {
@@ -31,6 +31,7 @@ Athlete.prototype.hold = function() {
   return false
   } else {
     this.playerScore = this.playerScore += this.turnTotal;
+    this.turnTotal = 0;
     return true
   }
 
@@ -43,3 +44,31 @@ Athlete.prototype.score = function() {
     return false;
   }
 }
+
+
+
+$(function() {
+  $("#form1").submit(function(event){
+    event.preventDefault();
+    player1 = new Athlete($("input#player1").val());
+    player2 = new Athlete($("input#player2").val());
+  });
+  $("#p1Roll").click(function(){
+    player1.diceRoll();
+    $("#player1roll").text("Roll:" + player1.roll);
+    $("#player1turn").text("Total:" + player1.turnTotal);
+  });
+  $("#p2Roll").click(function(){
+    player2.diceRoll();
+    $("#player2roll").text("Roll:" + player2.roll);
+    $("#player2turn").text("Total:" + player2.turnTotal);
+  });
+  $("#p1Hold").click(function(){
+    player1.hold();
+    $("#player1hold").text("Score:" + player1.playerScore);
+
+  });
+  $("#p2Hold").click(function(){
+    player2.hold();
+  });
+});
