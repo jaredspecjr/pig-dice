dc026a1ac5ad131daaa7636d77b2f54325c059d9
jared <jaredspecjr@gmail.com>
2018-04-06 00:43:14 -0700

    complete front end logic and add ability to reset game when done

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ Athlete.prototype.hold = function() {
 }
 
 Athlete.prototype.score = function() {
-  if(this.playerScore >= 100) {
+  if(this.playerScore >= 10) {
     return true;
   } else {
     return false;
@@ -54,27 +54,52 @@ $(function() {
     player2 = new Athlete($("input#player2").val());
     $("#player1name").text($("input#player1").val());
     $("#player2name").text($("input#player2").val());
+    if(player1.title === "" || player2.title === ""){
+      return false
+    } else {
+      $("#row1").hide();
+    };
+
+
   });
   $("#p1Roll").click(function(){
     player1.diceRoll();
     $("#player1roll").text("Roll:" + player1.roll);
     $("#player1turn").text("Total:" + player1.turnTotal);
+    if(player1.roll === 0){
+      $(".p2buttons").show();
+      $(".p1buttons").hide();
+    };
   });
   $("#p2Roll").click(function(){
     player2.diceRoll();
     $("#player2roll").text("Roll:" + player2.roll);
     $("#player2turn").text("Total:" + player2.turnTotal);
+    if(player2.roll === 0){
+      $(".p1buttons").show();
+      $(".p2buttons").hide();
+    };
   });
   $("#p1Hold").click(function(){
     player1.hold();
     $("#player1hold").text("Score:" + player1.playerScore);
+    $(".p2buttons").show();
+    $(".p1buttons").hide();
     player1.score();
     if(player1.score() === true){
-      alert("you won yo");
-    }
+      alert(player1.title + " " + "Won!!!");
+      location.reload();
+    };
   });
   $("#p2Hold").click(function(){
     player2.hold();
     $("#player2hold").text("Score:" + player2.playerScore);
+    $(".p1buttons").show();
+    $(".p2buttons").hide();
+    player2.score();
+    if(player2.score() === true){
+      alert(player2.title + " " + "Won!!!");
+      location.reload();
+    };
   });
 });
